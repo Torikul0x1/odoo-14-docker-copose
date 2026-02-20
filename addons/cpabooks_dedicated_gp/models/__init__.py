f773b177e25cb227a391dbca1d5e321005fef4be
Kamrul <kamrul.hasan@brainstation-23.com>
2026-02-21 02:06:55 +0600

    [ADD] docker

---
+++ addons/cpabooks_dedicated_gp/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import invoice
+from . import product_class
+from . import product_template
+from . import sale_order
+from . import product_product
---
+++ addons/cpabooks_field_serviece_extend/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import project_task
+from . import contact_person
+from . import site_location
+from . import material_request_line
